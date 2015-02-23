0b0d4a06832840da7ac13b189d81fb894edb6a0e
shirleylu <xlu2@wellesley.edu>
2015-02-23 02:02:42 -0500

    added functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var pomo = "<input type='text' placeholder='watch covert affairs' id='goal' value='watch covert affairs'><br><button id='go' onclick='pomogo()'>pomoGO</button>";
+var pomo = "<input type='text' placeholder='what is your next pomogranate?' id='goal'><br><button id='go' onclick='pomogo()'>pomoGO</button>";
 //
 //var currPomo = "<h2>Current Pomogranate</h2><div id='currGoal'></div><div id='currTimer'></div><div id='ratePomo'></div><button onclick='newPomo()' id='newpom' hidden>New Pomogranate</button>";
 //
@@ -9,8 +9,14 @@ $( document ).ready(function() {
 function pomogo () {
   var pomogoal = $('#goal').val();
   $('#goal').remove();
-  $('#pomo').html("<div id='pomogoal'>"+pomogoal+"</div><div id='pomoct'>25:00</div>");
+  $('#pomo').html("<div id='pomogoal'>"+pomogoal+"</div><div id='pomoct'>25:00</div><button id='done' onclick='pomodonearly()'>done early!</button>");
   pomocount();
+}
+
+function pomodonearly () {
+  clearInterval(timer);
+  pomono();
+  pomoplay(timeLeft);
 }
 
 function pomono () {
@@ -18,16 +24,22 @@ function pomono () {
   $('#pomo').append(pomodone);
 }
 
+var disab = true;
+
 function pomorate () {
   var ratepomo = "<div id='ratepomo'><p>How was your pomogranate?</p><span id='seed1' onClick='pomonom(1)'><img class='seed' src='media/seed0.png'/></span><span id='seed2' onClick='pomonom(2)'><img class='seed' src='media/seed0.png'/></span><span id='seed3' onClick='pomonom(3)'><img class='seed' src='media/seed0.png'/></span><span id='seed4' onClick='pomonom(4)'><img class='seed' src='media/seed0.png'/></span><span id='seed5' onClick='pomonom(5)'><img class='seed' src='media/seed0.png'/></span><span id='seeded' hidden>0</span></div>";
   $('#repomo').html(ratepomo);
   var pomonew = "<button id='pomonew' onclick='pomonew(true)'>Another Pomogranate</button>";
   $('#repomo').append(pomonew);
+  if (disab)
+    $('#pomonew').attr('disabled','disabled');
 }
 
 function pomoagain () {
   var pomonew = "<button id='pomonew' onclick='pomonew(false)'>Another Pomogranate</button>";
   $('#repomo').html(pomonew);
+  if (disab)
+    $('#pomonew').attr('disabled','disabled');
 }
 
 function pomonew (tf) {
@@ -59,13 +71,20 @@ function compomo (yn) {
   }
 }
 
+var timeLeft; 
+var timer;
+
 function pomocount () {
   var time = 1499000;
-  var timeLeft = time;
-  var timer = setInterval(function() {
+//  var time = 1000;
+  timeLeft = time;
+  timer = setInterval(function() {
     if (timeLeft == 0){
       clearInterval(timer);
+      var audio = new Audio('http://helios.informatik.uni-kl.de/~c_schrei/old/misc/sounds/ALARM.WAV');
+      audio.play();
       pomono();
+      pomoplay();
     }
     var secs = timeLeft / 1000;
     var sec = "0"+(secs % 60);
@@ -75,7 +94,23 @@ function pomocount () {
   },1000);
 }
 
-//function addCompPomo(goal,success) {
-//  var sucs = ["<img class='seed' src='media/seed1.png'/><img class='seed' src='media/seed0.png'/><img class='seed' src='media/seed0.png'/><img class='seed' src='media/seed0.png'/><img class='seed' src='media/seed0.png'/>","<img class='seed' src='media/seed1.png'/><img class='seed' src='media/seed2.png'/><img class='seed' src='media/seed0.png'/><img class='seed' src='media/seed0.png'/><img class='seed' src='media/seed0.png'/>","<img class='seed' src='media/seed1.png'/><img class='seed' src='media/seed2.png'/><img class='seed' src='media/seed3.png'/><img class='seed' src='media/seed0.png'/><img class='seed' src='media/seed0.png'/>","<img class='seed' src='media/seed1.png'/><img class='seed' src='media/seed2.png'/><img class='seed' src='media/seed3.png'/><img class='seed' src='media/seed4.png'/><img class='seed' src='media/seed0.png'/>","<img class='seed' src='media/seed1.png'/><img class='seed' src='media/seed2.png'/><img class='seed' src='media/seed3.png'/><img class='seed' src='media/seed4.png'/><img class='seed' src='media/seed5.png'/>"];
-//  $('#pomos').append("<div class='pomo'>"+goal+sucs[success-1]+"</div>");
-//}
+function pomoplay (extra) {
+  extra = typeof extra !== 'undefined' ? extra : 0;
+  var time = 299000;
+//  var time = 5000;
+  timeLeft = time + extra;
+  timer = setInterval(function() {
+    if (timeLeft == 0){
+      clearInterval(timer);
+      var audio = new Audio('http://helios.informatik.uni-kl.de/~c_schrei/old/misc/sounds/ALARM.WAV');
+      audio.play();
+      $('#pomonew').removeAttr('disabled');
+      disab = false;
+    }
+    var secs = timeLeft / 1000;
+    var sec = "0"+(secs % 60);
+    var min = "0"+(Math.floor(secs / 60));
+    $('#pomoct').html(min.slice(-2)+":"+sec.slice(-2));
+    timeLeft -= 1000;
+  },1000);
+}
